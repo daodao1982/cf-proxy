9134808d54e4b4f02ef4cc8bc1c0a13416e40626
daodao1982 <52167562+daodao1982@users.noreply.github.com>
2024-05-23 14:03:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="226.tuzi226.top";
+        url.hostname="https://226.tuzi226.top";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
